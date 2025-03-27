02ba89ffea069f5d7a0459c0dffb0d19cf6dc80e
kesanaChinni <kesanagowthami067@gmail.com>
2025-03-28 00:46:32 +0530

    Initial commit - Added weather app project

---
+++ frontend/src/Components/ForeCastCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Pages/Home.css";
+
+const ForeCastCard = ({ forecastData }) => {
+  if (forecastData.length === 0) return null;
+
+  return (
+    <div className="forecast-container">
+      <h2 className="forecast-title">Forecast</h2>
+      <div className="forecast-grid">
+        {forecastData.map((day, index) => (
+          <div key={index} className="forecast-card">
+            <p className="forecast-date">{day.date}</p>
+            <img src={day.weatherType} alt="Weather Icon" className="forecast-icon" />
+            <p className="forecast-temp">{day.temperature}°C</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ForeCastCard;
